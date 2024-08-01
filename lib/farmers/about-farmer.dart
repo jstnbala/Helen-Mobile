7867c3512e248560ea63efb0aefd6788ecf6fffd
Justin <justinbala339@gmail.com>
2024-08-01 18:31:11 +0800

    farmer orders, sidebar, about, help ui

--- lib/farmers/about-farmer.dart
+++ lib/farmers/about-farmer.dart
@@ -5,7 +5,7 @@ class AboutFarmer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xFF0C7230),
+        backgroundColor: Color(0xFFCA771A),
         leading: IconButton(
           icon: Icon(Icons.arrow_back, color: Colors.white),
           onPressed: () => Navigator.of(context).pop(),
@@ -15,76 +15,39 @@ class AboutFarmer extends StatelessWidget {
           style: TextStyle(
             fontFamily: 'Poppins',
             fontWeight: FontWeight.bold,
-            color: Colors.white, // Set the text color to white
+            color: Colors.white,
           ),
           textAlign: TextAlign.center,
         ),
-        centerTitle: true, // Center the title horizontally
+        centerTitle: true,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(20),
+          ),
+        ),
       ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 16.0),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12.0),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black26,
-                    blurRadius: 5.0,
-                    offset: Offset(0, 2),
-                  ),
-                ],
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(12.0),
-                child: Image.asset(
-                  'images/farmers/Frame 5.png',
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            Text(
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dignissim, nibh ac dictum finibus, tellus enim porttitor ex,',
-              style: TextStyle(
-                fontFamily: 'Poppins',
-                fontWeight: FontWeight.normal,
-                color: Colors.black,
-              ),
-              textAlign: TextAlign.justify,
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 16.0),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12.0),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black26,
-                    blurRadius: 5.0,
-                    offset: Offset(0, 2),
-                  ),
-                ],
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(12.0),
-                child: Image.asset(
-                  'images/farmers/lucbantradingpost.jpg',
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            Text(
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dignissim, nibh ac dictum finibus, tellus enim porttitor ex,',
-              style: TextStyle(
-                fontFamily: 'Poppins',
-                fontWeight: FontWeight.normal,
-                color: Colors.black,
-              ),
-              textAlign: TextAlign.justify,
-            ),
-          ],
+      body: PageView(
+        children: [
+          buildImageContainer('images/farmers/about-cover.jpg'),
+          buildImageContainer('images/farmers/vision-mission.jpg'),
+          buildImageContainer('images/farmers/dev-goals.jpg'),
+        ],
+      ),
+    );
+  }
+
+  Widget buildImageContainer(String imagePath) {
+    return Container(
+      margin: EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(color: Color(0xFFCA771A), width: 3),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Image.asset(
+          imagePath,
+          fit: BoxFit.contain,
         ),
       ),
     );
